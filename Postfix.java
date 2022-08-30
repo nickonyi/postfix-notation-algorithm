e387f36e9945c7b428d0fba61a4f485f05ed72c2
nickonyi <nickoluoch1130@gmail.com>
2022-08-30 16:17:02 +0300

    Postfix expression

--- Postfix.java
+++ Postfix.java
@@ -1,70 +1,66 @@
 import java.util.*;
 
 public class Postfix {
-    //method to evaluate post fix notation
-    public static int evaluatePostfix(String exp){
-//create a stack
-Stack<Integer> stack = new Stack<>();
-//Scan all the characters one by one
-for(int i=0; i<exp.length();i++){
-  char c = exp.charAt(i);
-  //if the character is an operand push it to the stack  
-  if(Character.isDigit(c)){
-      stack.push(c-'0');
-      
-    } 
-    //if the scanned character is an operator 
-//pop two elements from the stack and apply operator
-    else {
-      int val1 = stack.pop();
-      int val2 = stack.pop();
+  String currentExpr;
+  public static void main (String[] args){
+    Postfix evalPostfix = new Postfix();
+    evalPostfix.setCurrentExpr(evalPostfix.getUserString());
+    System.out.println("We are evaluating "+evalPostfix.currentExpr);
+    evalPostfix.controller(evalPostfix.currentExpr);
+  }
 
-      switch(c){
-        case '+':
-        stack.push(val2+val1);
-        break;
-        case '-':
-        stack.push(val2-val1);
-        break;
-        case '*':
-        stack.push(val2*val1);
-        break;
-        case '/':
-        stack.push(val2/val1);
-        break;
+  String getUserString(){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the postfix expression you want to evaluate");
+    String strExpr = in.nextLine();
+    in.close();
 
-      }
+    return strExpr;
+  }
+private void setCurrentExpr(String enteredExpression){
+      currentExpr = enteredExpression;
+}
+
+void controller (String postFixExpr){
+Stack<String> myStack1 = tokenizeExpr(postFixExpr);
+System.out.println(myStack1);
+Stack<String> mystack2 = new Stack<>();//for storing intermediate results
+}
+
+private Stack<String> tokenizeExpr(String prfev){
+  prfev.trim();//removing white spaces in our evaluation
+  Stack<String> rtnStack = new Stack<>();
+  //store the contents of the string in reverse order
+  for(int i=prfev.length(); i>0; i--){
+      rtnStack.push(prfev.substring(i-1,i));
+
+  }
+
+  return rtnStack;
+}
+
+void evaluatePostfix(Stack<String> myStack1,Stack<String> myStack2){
+  //this method will recursively call itself until it has evaulated the entered expression
+  if(myStack1.size()==0 && myStack2.size()==1){
+      System.out.println("Result is " + myStack2);
+  } else {
+    //read the element from the string and see if it can be an operand
+    String itemPicked = myStack1.pop();
+  }
+
+ 
+
+}
+private String checkType(String pop){
+    String foundType = "";
+    try {
+      int var = Integer.parseInt(pop);
+      foundType = "opd";
+    } catch(Exception e){
+      foundType = "opr";
+      System.out.println("Error in number format"+e.toString());
     }
-    }
 
-return stack.pop();
-    }
-
-    //performs the push operation
-    public static void pushElement(Stack<Integer> stack1,int x){
-      //invoke the stack method
-      stack1.push(x);
-      System.out.println("Push->"+x );
-      //print the modified stack
-      System.out.println("Stack" + stack1);
-
-      }
-
-      public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your Postfix Expression");
-        String exp = input.nextLine();
-        System.out.println("Postfix Evaluation:"+evaluatePostfix(exp));
-        
-      }
-   
-              
-
-              
-            }
-
-         
-
-   
-
-
+    return foundType;
+}
+}
